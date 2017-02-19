2c0d575980bb2a460a99da482a6fb576b64e714e
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-02-18 23:58:01 -0600

    views complete

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ var express = require("express");
 var app = express();
 
 var path = require("path");
+//static content for the app from the "public" directory
 app.use(express.static(process.cwd + "/public"));
 
+//override with POST having ?_method=DELETE
 var methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended:false }));
 
+//set handlebars
 var handlebars = require("express-handlebars");
 app.engine("handlebars", handlebars({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+//import routes and give the server access to tehm
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
