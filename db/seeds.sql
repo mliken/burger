a0c37cda46fd332daf0442052fddc6aaed4defa1
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-02-19 18:29:07 -0600

    fixed database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Cheddar and Bacon", true)
-,("Mushroom and Swiss", false)
-,("Chicken, Bacon, Ranch", false)
-
-SELECT * FROM burgers;
+INSERT INTO burgers
+(burger_name, devoured)
+VALUES
+('Swiss Mushroom Burger', TRUE)
+,('Bacon Cheddar Burger', FALSE)
+,('Buffalo Chicken Burger', TRUE);
